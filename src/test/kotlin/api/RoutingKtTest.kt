c6148f91c9eea0e2d514f9c0cacc6189c56cbe91
Loic Mesieres <loic.mesieres@gmail.com>
2024-04-09 23:10:44 +0200

    feat: add mongo repo

--- src/test/kotlin/api/RoutingKtTest.kt
+++ src/test/kotlin/api/RoutingKtTest.kt
@@ -1,21 +1,41 @@
 package api
 
+import com.mongodb.kotlin.client.coroutine.MongoClient
 import io.kotest.assertions.ktor.client.shouldHaveStatus
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
 import io.ktor.server.testing.*
+import org.koin.dsl.module
+import org.koin.ktor.plugin.Koin
+import org.testcontainers.containers.MongoDBContainer
 import org.tournament.api.CreatePlayerApi
 import org.tournament.api.UpdatePlayerScoreApi
 import org.tournament.api.configureRouting
+import org.tournament.appModule
 import org.tournament.configureContentNegotiation
-import org.tournament.configureKoin
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class RoutingKtTest {
+
+    private val mongo = MongoDBContainer("mongo:latest").apply {
+        startupAttempts = 1
+    }
+    val mongoRunning = mongo.start()
+
+    fun Application.configureKoin() {
+        install(Koin) {
+            modules(
+                module {
+                    single { MongoClient.create(mongo.replicaSetUrl) }
+                    single { get<MongoClient>().getDatabase("tournament") }
+                }, appModule)
+        }
+    }
 
     @Test
     fun `get players list should return empty array`() = testApplication {
